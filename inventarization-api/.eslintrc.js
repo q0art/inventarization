d51732bfbcf171e22d7112105f42fdd30682f79c
merkulov artem <wzx.d.art@gmail.com>
2024-05-14 00:21:54 +0300

    add prisma schema and controller decorators

---
+++ inventarization-api/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module"
+  },
+  plugins: ["simple-import-sort"],
+  extends: ["plugin:prettier/recommended"],
+  root: true,
+  env: { node: true },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    //
+    "prettier/prettier": ["error", { endOfLine: "auto" }]
+  }
+};
---
+++ inventarization-api/src/main.ts
@@ -0,0 +1,19 @@
+import { AppModule } from "@app";
+import { NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Authorization", "Content-Type"],
+  });
+
+  await app.listen(3000);
+};
+
+bootstrap();
